eb7ab2c376086c68f839596a595c5384ba763523
kurnik <c.jaworski@fu-berlin.de>
2024-03-02 22:03:17 +0100

    feat: Update User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates username, presence: true
-  validates email, presence: true
-  validates password, presence: true
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true
 end
